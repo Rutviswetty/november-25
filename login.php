5e23c5f377c719143c1cb5405b64b4971b8fd7c3
Rutviswetty <148977289+Rutviswetty@users.noreply.github.com>
2023-11-02 17:52:12 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,30 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+<style>
+    .login-bg img{
+        height: 100%;
+        width: 100%;
+        object-fit: cover;
+      
+    }
+</style>
+
+<Section>
+    <div class="container">
+        <div class="row">
+            <div class="col-6">
+                <div class="login-bg">
+                    <img src="img/login-bg.png" alt="">
+                </div>
+            </div>
+            <div class="col-6">
+                <div class="login-heading">
+                    <h5>Regiter Here</h5>
+                    <p>Register one-time, so you can log in in the future. </p>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</Section>
